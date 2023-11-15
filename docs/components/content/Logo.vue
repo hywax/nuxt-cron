b4761c42eaec464514ae004beb564f00cbbdbb93
hywax <a.hywax@gmail.com>
2023-11-15 10:30:31 +0500

    Add `module` logotype

---
+++ docs/components/content/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="logo">
+    <img src="/icon.svg" alt="" />
+    <span>
+      Nuxt
+      <i>Cron</i>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .logo {
+    display: flex;
+    align-items: center;
+    gap: var(--space-2);
+
+    img {
+      height: 1.8rem;
+    }
+
+    span {
+      font-size: var(--fontSize-2xl);
+      font-weight: var(--fontWeight-bold);
+
+      i {
+        color: var(--color-primary-500);
+        font-style: normal;
+      }
+    }
+  }
+</style>
