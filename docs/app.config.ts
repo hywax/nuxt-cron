e0a6049753ec1a4cd0d9f48762ab7795192e1377
hywax <a.hywax@gmail.com>
2023-11-15 10:29:37 +0500

    Add base structure for docs

---
+++ docs/app.config.ts
@@ -0,0 +1,35 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt Cron',
+    description: 'A Nuxt module for cron jobs in your app.',
+    image: '/cover.jpg',
+    socials: {
+      twitter: 'hywax_',
+      github: 'hywax/nuxt-cron'
+    },
+    github: {
+      dir: 'docs/content',
+      branch: 'main',
+      repo: 'nuxt-cron',
+      owner: 'hywax',
+      edit: true
+    },
+    aside: {
+      level: 1,
+      collapsed: false,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+    footer: {
+      credits: {
+        icon: 'IconHywax',
+        text: 'Powered by Hywax',
+        href: 'https://hywax.space'
+      }
+    }
+  }
+})
---
+++ docs/nuxt.config.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  extends: '@nuxt-themes/docus',
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/icon.svg'
+        }
+      ]
+    }
+  }
+})
