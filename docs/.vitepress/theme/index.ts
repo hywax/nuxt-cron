6090bf3d62e6178c246919d86e4d2671a0dc79b4
hywax <a.hywax@gmail.com>
2024-01-10 16:29:08 +0500

    Added Yandex.Metrika for documentation

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,22 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import { yandexMetrika } from '@hywax/vitepress-yandex-metrika'
 import Sandbox from '../components/sandbox.vue'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('Sandbox', Sandbox)
+  enhanceApp(ctx) {
+    ctx.app.component('Sandbox', Sandbox)
+
+    yandexMetrika(ctx, {
+      enabled: import.meta.env.MODE === 'production',
+      counter: {
+        id: 96079193,
+        initParams: {
+          trustedDomains: ['nuxt-cron.hywax.space'],
+        },
+      },
+    })
   },
 } satisfies Theme
