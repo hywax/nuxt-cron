08e8efbfbbe5a1a1a0a6eb4aa94c3159e5193039
hywax <me@hywax.space>
2024-03-23 14:37:11 +0500

    chore: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,35 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  typescript: true,
-  vue: {
-    overrides: {
-      'vue/block-order': ['error', {
-        order: ['template', 'script', 'style'],
-      }],
+export default antfu(
+  {
+    typescript: true,
+    ignores: [
+      '.github',
+      '.output',
+      'dist',
+      'node_modules',
+    ],
+    vue: {
+      overrides: {
+        'vue/block-order': ['error', {
+          order: ['template', 'script', 'style'],
+        }],
+      },
     },
   },
-  rules: {
-    'style/brace-style': ['error', '1tbs'],
-    'style/arrow-parens': ['error', 'always'],
-    'no-console': 'off',
-    'curly': ['error', 'all'],
-    'antfu/consistent-list-newline': 'off',
+  {
+    rules: {
+      'style/brace-style': ['error', '1tbs'],
+      'style/arrow-parens': ['error', 'always'],
+      'no-console': 'off',
+      'curly': ['error', 'all'],
+      'antfu/consistent-list-newline': 'off',
+    },
   },
-}, {
-  ignores: [
-    '.github',
-    '.output',
-    'dist',
-    'node_modules',
-  ],
-})
+  {
+    files: ['package.json'],
+    rules: {
+      'style/eol-last': 'off',
+    },
+  },
+)
